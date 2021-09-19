bc4f4b66b16714bcf562793aa5f4e4cfbf4382ba
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2021-09-19 08:55:24 +0200

    Add more initial files

---
+++ emuzone-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: The Emulator Zone plugin
+Plugin URI: https://github.com/ghdpro/emuzone-plugin
+Description: WordPress plugin for The Emulator Zone site features
+Version: 0.1
+Author: ghdpro
+Author URI: https://www.emulator-zone.com/
+License: AGPL v3.0
+*/
