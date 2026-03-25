ab524ad4e665d5c571f9702a33cf29a4a199478d
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2026-03-25 19:37:30 +0100

    Escape all data input in template

--- templates/ezdownloads-active.php
+++ templates/ezdownloads-active.php
@@ -3,11 +3,11 @@
 <?php $this->display_message(); ?>
 
 <form action="<?php echo esc_html( admin_url( 'admin-post.php' ) ); ?>" method="post">
-	<input type="hidden" name="action" value="<?=$this->get_menu_slug()?>">
-	<input type="hidden" name="form_action" value="<?=$action?>">
+	<input type="hidden" name="action" value="<?=esc_attr( $this->get_menu_slug() )?>">
+	<input type="hidden" name="form_action" value="<?=esc_attr( $action )?>">
 	<?php wp_nonce_field( $this->get_menu_slug() . $action ); ?>
-	<input type="hidden" name="id" value="<?=$item->id?>'">
-	Are you sure you want to set the active download for handle <b><?=$item->handle?></b> to <b><?=$item->filename?></b>?
+	<input type="hidden" name="id" value="<?=esc_attr( $item->id) ?>'">
+	Are you sure you want to set the active download for handle <b><?=esc_attr( $item->handle )?></b> to <b><?=esc_attr( $item->filename )?></b>?
 	<table class="form-table">
 		<tbody>
 		<tr>
--- templates/ezdownloads-form.php
+++ templates/ezdownloads-form.php
@@ -3,19 +3,19 @@
 <?php $this->display_message(); ?>
 
 <form action="<?php echo esc_html( admin_url( 'admin-post.php' ) ); ?>" method="post">
-	<input type="hidden" name="action" value="<?=$this->get_menu_slug()?>">
+	<input type="hidden" name="action" value="<?=esc_attr( $this->get_menu_slug() )?>">
 	<input type="hidden" name="form_action" value="<?=$action?>">
 	<?php wp_nonce_field( $this->get_menu_slug() . $action ); ?>
-	<input type="hidden" name="id" value="<?=$item->id?>">
+	<input type="hidden" name="id" value="<?=esc_attr( $item->id )?>">
 	<table class="form-table">
 		<tbody>
 			<tr>
 				<th scope="row"><label for="version">Version</label></th>
-				<td><input name="version" id="version" type="text" class="regular-text" value="<?php echo $item->version ?? ''; ?>"></td>
+				<td><input name="version" id="version" type="text" class="regular-text" value="<?php echo esc_attr( $item->version ?? '' ); ?>"></td>
 			</tr>
 			<tr>
 				<th scope="row"><label for="release_date">Release date</label></th>
-				<td><input name="release_date" id="release_date" type="text" class="regular-text" value="<?php echo $item->release_date ?? date( 'Y-m-d' ); ?>">
+				<td><input name="release_date" id="release_date" type="text" class="regular-text" value="<?php echo esc_attr( $item->release_date ?? date( 'Y-m-d' ) ); ?>">
 				<span class="descriptiopn" style="color: #A0A5AA"><br>The release date of the emulator download. Defaults to today's date, but please change to match actual date.</span></td>
 			</tr>
 			<tr>
@@ -33,11 +33,11 @@
 		<tbody>
 			<tr>
 				<th scope="row"><label for="name">Name</label></th>
-				<td><input name="name" id="name" type="text" class="regular-text" required value="<?php echo $item->name ?? ''; ?>"></td>
+				<td><input name="name" id="name" type="text" class="regular-text" required value="<?php echo esc_attr( $item->name ?? '' ); ?>"></td>
 			</tr>
 			<tr>
 				<th scope="row"><label for="description">Description</label></th>
-				<td><input name="description" id="description" type="text" class="large-text" value="<?php echo $item->description ?? ''; ?>"></td>
+				<td><input name="description" id="description" type="text" class="large-text" value="<?php echo esc_attr( $item->description ?? '' ); ?>"></td>
 			</tr>
 			<tr>
 				<th scope="row"><label for="platform">Platform</label></th>
@@ -59,11 +59,11 @@
 			</tr>
 			<tr>
 				<th scope="row"><label for="homepage1_url">Homepage URL</label> <span class="dashicons dashicons-admin-home"></span></th>
-				<td><input name="homepage1_url" id="homepage1_url" type="text" class="large-text" value="<?php echo $item->homepage1_url ?? ''; ?>"></td>
+				<td><input name="homepage1_url" id="homepage1_url" type="text" class="large-text" value="<?php echo esc_attr( $item->homepage1_url ?? '' ); ?>"></td>
 			</tr>
 			<tr>
 				<th scope="row"><label for="source1_url">Source URL</label> <span class="dashicons dashicons-admin-site-alt3"></span></th>
-				<td><input name="source1_url" id="source1_url" type="text" class="large-text" value="<?php echo $item->source1_url ?? ''; ?>">
+				<td><input name="source1_url" id="source1_url" type="text" class="large-text" value="<?php echo esc_attr( $item->source1_url ?? '' ); ?>">
 				<span class="descriptiopn" style="color: #A0A5AA"><br>This URL is used to automatically match new downloads in the future. URL should be specific to the emulator, but not specific to a particular version.</span></td>
 			</tr>
 		</tbody>
